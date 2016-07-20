8c433feed305b475c5da4030a6a50355412b6003
Theresa Morcos <theresa.morcos90@gmail.com>
2016-07-20 21:17:30 +0100

    change host and port to work for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 
-var port = "8000";
-var host = "0.0.0.0";
+var port = process.env.PORT || "8000";
+var host = process.env.HOST || "0.0.0.0";
 var Express = require('express');
 
 var app = Express();
